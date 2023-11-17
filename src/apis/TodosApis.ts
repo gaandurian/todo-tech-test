508e5b28a80b7932cea823a7874487c49798c9bb
Mahmoud Ghandour <ghan.mahmoud@gmail.com>
2023-11-17 21:12:49 +0100

    Hotfix on fetch api

--- src/apis/TodosApis.ts
+++ src/apis/TodosApis.ts
@@ -1,6 +1,5 @@
 export const getAllTodos = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users/1/todos")
-    const data = await response.json()
-    return data
+    return response.json()
   } 
   
